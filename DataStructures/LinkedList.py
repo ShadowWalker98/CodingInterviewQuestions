09a1b8df2519c0cfb8600a8de990077575a5967a
ShadowWalker98 <44528434+ShadowWalker98@users.noreply.github.com>
2020-08-03 18:29:18 +0530

    added insert at end

--- DataStructures/LinkedList.py
+++ DataStructures/LinkedList.py
@@ -14,24 +14,76 @@ class SLinkedList:
         while printval is not None:
             print (printval.dataval, end="->")
             printval = printval.nextval
+        print("\n")
 
-# creating list
-list1 = SLinkedList()
+    def insertAtBeginning(self, data):
+        if self.headval == None:
+            # the linked list is empty
+            # hence make new node and set headval
+            nodeToInsert = Node(data)
+            self.headval = nodeToInsert
+        else:
+            # case where the list is not empty
+            # the headval points to the next node
+            # store that reference somewhere
+            # storing it in nodeLink
+            nodeLink = self.headval
+            # creating the new node
+            newNode = Node(data)
+            # setting the head of list to the created node
+            self.headval = newNode
+            # setting nextval of new node to the previous nodes in the
+            newNode.nextval = nodeLink
 
-print(" enter num of entries: \n")
-n = int(input())
-currentLastNode = None
-for i in range(n):
+    def insertAtEnd(self, data):
+        if self.headval == None:
+            self.insertAtBeginning(data)
+        else:
+            # traverse till the end of the list
+            currentNode = self.headval
+            while currentNode.nextval != None:
+                currentNode = currentNode.nextval
+            # currentNode now points to the last node
+            # create the new node
+            newNode = Node(data)
+            # adding new node to the end of the list
+            currentNode.nextval = newNode
 
-    num = int(input("Enter num"))
-    if list1.headval == None:
-        firstNode = Node(num)
-        list1.headval = firstNode
-        currentLastNode = list1.headval
-    else:
-        # creating the new Node
-        newNode = Node(num)
-        currentLastNode.nextval = newNode
-        currentLastNode = newNode
-print("\n")
-list1.listprint()
+
+
+
+if __name__=="__main__":
+    list1 = SLinkedList()
+    num = input("Enter num\n")
+    list1.insertAtBeginning(num)
+    list1.listprint()
+    num = input("Enter num\n")
+    list1.insertAtBeginning(num)
+    list1.listprint()
+    num = input("Enter num\n")
+    list1.insertAtEnd(num)
+    list1.listprint()
+    num = input("Enter num\n")
+    list1.insertAtEnd(num)
+    list1.listprint()
+
+    # print(" enter num of entries: \n")
+    # n = int(input())
+    # currentLastNode = None
+    # for i in range(n):
+
+    #     num = int(input("Enter num"))
+    #     if list1.headval == None:
+    #         firstNode = Node(num)
+    #         list1.headval = firstNode
+    #         currentLastNode = list1.headval
+    #     else:
+    #         # creating the new Node
+    #         newNode = Node(num)
+    #         currentLastNode.nextval = newNode
+    #         currentLastNode = newNode
+    # print("\n")
+    # # printing list
+    # list1.listprint()
+
+
