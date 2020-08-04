c049bb94237eb7ef090e9c44ac420085f0bfe583
ShadowWalker98 <44528434+ShadowWalker98@users.noreply.github.com>
2020-08-04 13:43:50 +0530

    added Selection Sort

---
+++ Sorting/SelectionSort.py
@@ -0,0 +1,12 @@
+# implementation of selection sort
+
+def selectionSort(array):
+    currentIdx = 0
+    while currentIdx < len(array) - 1:
+        smallestIdx = currentIdx
+        for i in range(currentIdx + 1, len(array)):
+            if(array[smallestIdx] > array[i]):
+                smallestIdx = i
+        array[currentIdx], array[smallestIdx] = array[smallestIdx], array[currentIdx]
+        currentIdx += 1
+    return array
